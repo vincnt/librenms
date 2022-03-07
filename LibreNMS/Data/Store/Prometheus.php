a99e081260af12e2959ab739cebef73bb20ca8f4
Vincent Tan <vincnttan@gmail.com>
2022-03-07 18:16:19 +0000

    make professional

--- LibreNMS/Data/Store/Prometheus.php
+++ LibreNMS/Data/Store/Prometheus.php
@@ -36,23 +36,41 @@ use Log;
 class Prometheus extends BaseDatastore
 {
     private $client;
+    private $job;
     private $base_uri;
     private $default_opts;
     private $enabled;
     private $prefix;
-    private $group_values; #new
+    private $method;
+    private $prune_threshold_seconds;
+    private $metrics_dir;
 
     public function __construct(\GuzzleHttp\Client $client)
     {
         parent::__construct();
         $this->client = $client;
 
-        $job = Config::get('prometheus.job', 'librenms');
+        // shared config
+        $this->job = Config::get('prometheus.job', 'librenms');
         $this->prefix = Config::get('prometheus.prefix', '');
         if ($this->prefix) {
             $this->prefix = "$this->prefix" . '_';
         }
-        $prune_threshold_seconds = Config::get('prometheus.prune_threshold_seconds', 300);
+        $this->method = Config::get('prometheus.method', 'pushgateway');
+
+        // pushgateway config
+        $url = Config::get('prometheus.url');
+        $this->base_uri = "$url/metrics/job/$job/instance/";
+        $this->default_opts = [
+            'headers' => ['Content-Type' => 'text/plain'],
+        ];
+        if ($proxy = Proxy::get($url)) {
+            $this->default_opts['proxy'] = $proxy;
+        }
+
+        // local config
+        $this->prune_threshold_seconds = Config::get('prometheus.prune_threshold_seconds', 300);
+        $this->metrics_dir = Config::get('prometheus.metrics_dir', "/opt/librenms/prometheus_metrics/");
 
         $this->enabled = self::isEnabled();
     }
@@ -75,52 +93,123 @@ class Prometheus extends BaseDatastore
             return;
         }
 
-        $labels = array();
-        foreach ($tags as $t => $v) {
-            if ($v !== null) {
-                array_push($labels, "$t=\"" . addcslashes($v, '\\') . "\"");
-            }
+        if ($this->method == 'local'){
+            $this->runLocal($device, $measurement, $tags, $fields);
+        }
+        else {
+            $this->runPushGateway($device, $measurement, $tags, $fields);
         }
 
+
+        $this->recordStatistic($stat->end());
+    }
+
+    private function runPushGateway($device, $measurement, $tags, $fields)
+    {
+        try {
+            $vals = '';
+            $promtags = '/measurement/' . $measurement;
+
+            foreach ($fields as $k => $v) {
+                if ($v !== null) {
+                    $vals .= $this->prefix . "$k $v\n";
+                }
+            }
+
+            foreach ($tags as $t => $v) {
+                if ($v !== null) {
+                    $promtags .= (Str::contains($v, '/') ? "/$t@base64/" . base64_encode($v) : "/$t/$v");
+                }
+            }
+            $options = $this->getDefaultOptions();
+            $options['body'] = $vals;
+
+            $promurl = $this->base_uri . $device['hostname'] . $promtags;
+            if (Config::get('prometheus.attach_sysname', false)) {
+                $promurl .= '/sysName/' . $device['sysName'];
+            }
+            $promurl = str_replace(' ', '-', $promurl); // Prometheus doesn't handle tags with spaces in url
+
+            Log::debug("Prometheus put $promurl: ", [
+                'measurement' => $measurement,
+                'tags' => $tags,
+                'fields' => $fields,
+                'vals' => $vals,
+            ]);
+
+            $result = $this->client->request('POST', $promurl, $options);
+
+            if ($result->getStatusCode() !== 200) {
+                Log::error('Prometheus Error: ' . $result->getReasonPhrase());
+            }
+        } catch (GuzzleException $e) {
+            Log::error('Prometheus Exception: ' . $e->getMessage());
+        }
+    }
+
+    private function runLocal($device, $measurement, $tags, $fields)
+    {
+        if ($this->metrics_dir == null){
+           die("metrics_dir not set in config.php for prometheus");
+        }
+        if (!file_exists($this->metrics_dir)){
+           die("metrics_dir $this->metrics_dir doesn't exist. You need to create one manually");
+        }
+        // Build up labels
+        $labels = array();
         array_push($labels, "job=\"" . $this->job . "\"");
         array_push($labels, "instance=\"" . $device['hostname'] . "\"");
         array_push($labels, "measurement=\"" . addcslashes($measurement,'\\') . "\"");
         if (Config::get('prometheus.attach_sysname', false)) {
                 array_push($labels, "sysName=\"".$device['sysName']."\"");
         }
+        foreach ($tags as $tag => $value) {
+            if ($value !== null) {
+                array_push($labels, "$tag=\"" . addcslashes($value, '\\') . "\"");
+            }
+        }
 
-        $target_file = "/opt/librenms/prometheus_metrics/" . $device['hostname'] . "_" . $measurement . ".prom";
+        $target_file = $this->metrics_dir . $device['hostname'] . "_" . $measurement . ".prom";
         $group_values = array();
+        $current_time = time();
+
+
         // Check existing file for metrics and prune old ones
         if (file_exists($target_file)){
             $existing_lines = explode("\n", file_get_contents($target_file));
-            $prune_threshold_seconds = 300300300;
-            foreach ($existing_lines as $v){
-                if ($v !== null) {
-                    $items = explode(" ", $v);
-                    if (count($items)==3 && time() - $items[2]/1000 < $prune_threshold_seconds){
-                        $group_values[$items[0]] = $items[1] . " " . $items[2];
-                    }
+            foreach ($existing_lines as $line){
+                if ($line !== null) {
+                    $items = explode(" ", $line);
+                    if ($items !== null && count($items) == 3){
+                        $group = $items[0];
+                        $value = $items[1];
+                        $timestamp = $items[2] / 1000;
+                        if ($current_time - $timestamp < $this->prune_threshold_seconds){
+                            $group_values[$group] = "$value $timestamp";
+                       }
+                   }
                 }
             }
         }
+
         // Add or replace with the new metrics from this polling session
-        foreach ($fields as $k => $v) {
-            if ($v !== null) {
-                    $group = $this->prefix . $k . "{" . implode(',',$labels) ."}";
-                    $group_values[$group] = "$v " . time() . "000";
+        foreach ($fields as $key => $value) {
+            if ($value !== null) {
+                    $group = $this->prefix . $key . "{" . implode(',',$labels) ."}";
+                    $group_values[$group] = "$value " . $current_time . "000";
             }
         }
 
         $lines = '';
-        foreach ($group_values as $g => $v){
-            $lines .= $g . " $v\n";
+        foreach ($group_values as $group => $value){
+            $lines .= $group . " $value\n";
         }
 
-        $res = file_put_contents($target_file, $lines);
-        Log::info("Res: $res for writing metrics to $target_file");
+        $res = file_put_contents($target_file, $lines, LOCK_EX);
 
-        $this->recordStatistic($stat->end());
+        if ($res === false){
+           die("Unable to write to $target_file " . implode(",",error_get_last()));
+        }
     }
 
     private function getDefaultOptions()
